b0291723a0ce8e886ea72a2182c87dbb157b6475
Strongman332 <Strongman332@gmail.com>
2013-05-08 01:36:25 -0400

    updted map script

--- map.sh
+++ map.sh
@@ -1,1 +1,7 @@
-xalan -in map.xml -xsl map.xslt -out map.svg
+rm -r -f gh-pages
+git clone -b gh-pages https://github.com/Strongman332/Civcraft-Road-Map.git gh-pages
+xalan -in map.xml -xsl map.xslt -out gh-pages/map.svg
+cd gh-pages
+git push origin gh-pages
+cd ..
+rm -r -f gh-pages
