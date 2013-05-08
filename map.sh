39a2fe74d92f38b1f6c87e4d953f06d6d7a987d4
Strongman332 <Strongman332@gmail.com>
2013-05-08 12:17:42 -0300

    Update map.sh

--- map.sh
+++ map.sh
@@ -1,7 +1,13 @@
+echo Cleaning
 rm -r -f gh-pages
-git clone -b gh-pages https://github.com/Strongman332/Civcraft-Road-Map.git gh-pages
-xalan -in map.xml -xsl map.xslt -out gh-pages/map.svg
+echo Clone gh-pages from github
+git clone -q -b gh-pages https://github.com/Strongman332/Civcraft-Road-Map.git gh-pages
+echo Generating map.svg
+xalan -in map.xml -xsl map.xslt -out gh-pages/map.svg && clear
 cd gh-pages
-git push origin gh-pages
+echo Please log in
+((git push origin gh-pages||git push origin gh-pages||git push origin gh-pages) && (clear && echo SUCCESS!)) || (clear; echo FAILED!)
+echo Cleaning
 cd ..
 rm -r -f gh-pages
+read -p "Press enter to continue" nothing
