1b8b29f8dea57f285e7186a3d67c076229d328fb
Strongman332 <Strongman332@gmail.com>
2013-05-08 12:20:24 -0300

    Update map.sh

--- map.sh
+++ map.sh
@@ -1,7 +1,7 @@
 echo Cleaning
 rm -r -f gh-pages
 echo Clone gh-pages from github
-git clone -q -b gh-pages https://github.com/Strongman332/Civcraft-Road-Map.git gh-pages
+git clone -b gh-pages https://github.com/Strongman332/Civcraft-Road-Map.git gh-pages
 echo Generating map.svg
 xalan -in map.xml -xsl map.xslt -out gh-pages/map.svg && clear
 cd gh-pages
