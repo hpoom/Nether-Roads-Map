903be6c00ee5599587162ad0b981dc34acc7956d
Strongman332 <Strongman332@gmail.com>
2013-05-08 13:32:05 -0300

    Update BuildAndPublish.sh

--- BuildAndPublish.sh
+++ BuildAndPublish.sh
@@ -1,18 +1,26 @@
+#!/bin/bash
 #GenerateAndPublish.sh
 #
 #use to publish map to gh-pages (http://strongman332.github.io/Civcraft-Road-Map/)
 #use GenerateTestMap.sh for testing
 #
 #depends- xalan, git
+#
+
+TODAY=$(date +%D)
+HOUR=$(date +%T)
+
 
 echo Cleaning
 rm -r -f gh-pages
 echo Clone gh-pages from github
 git clone -b gh-pages https://github.com/Strongman332/Civcraft-Road-Map.git gh-pages
 echo Generating map.svg
-xalan -in map.xml -xsl map.xslt -out gh-pages/map.svg && clear
+xalan -in map.xml -xsl map.xslt -out gh-pages/map.svg
 cd gh-pages
-echo Please log in
+git add map.svg
+git commit -a -m "Auto Build $TODAY;$HOUR" && clear
+echo Please login to github
 ((git push origin gh-pages||git push origin gh-pages||git push origin gh-pages) && (clear && echo SUCCESS!)) || (clear; echo FAILED!)
 echo Cleaning
 cd ..
